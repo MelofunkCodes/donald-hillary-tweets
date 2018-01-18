3027fcdf517baeefc79d50c72b7921392c4293e2
Melinda Yeoh <myeoh27@gmail.com>
2018-01-18 17:36:50 -0500

    Created store and used it in Button Choices component by opting into its context

--- client/src/components/ButtonChoices.jsx
+++ client/src/components/ButtonChoices.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { browserHistory as history } from 'react-router';
+import PropTypes from 'prop-types';
 
-class ButtonChoices extends React.Component {
-  handleClick(username) {
-    history.push(`/tweets/${username}`);
+class ButtonChoices extends Component {
+  componentDidMount() {
+    const { store } = this.context;
+
+    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
+    const { store } = this.context;
+
     return (
       <div className="home-page">
         <div className="button-box">
@@ -14,13 +23,29 @@ class ButtonChoices extends React.Component {
             className="button-choice"
             src="https://user-images.githubusercontent.com/22921878/34543388-9cd4ab50-f0ae-11e7-85cf-16149b663883.gif"
             alt="Donald gif"
-            onClick={this.handleClick.bind(this, 'realDonaldTrump')}
+            onClick={() => {
+              const username = 'realDonaldTrump';
+              history.push(`/tweets/${username}`);
+
+              store.dispatch({
+                type: 'SELECT_USER',
+                user: username,
+              });
+            }}
           />
           <img
             className="button-choice"
             src="https://user-images.githubusercontent.com/22921878/34543432-c791607c-f0ae-11e7-915c-231df1c8961d.gif"
             alt="Hillary gif"
-            onClick={this.handleClick.bind(this, 'HillaryClinton')}
+            onClick={() => {
+              const username = 'HillaryClinton';
+              history.push(`/tweets/${username}`);
+
+              store.dispatch({
+                type: 'SELECT_USER',
+                user: username,
+              });
+            }}
           />
         </div>
         <div>
@@ -30,5 +55,17 @@ class ButtonChoices extends React.Component {
     );
   }
 }
+
+
+/*
+ Need to specify contextTypes to specify which context we want to receive (in this case, store
+ from Provider). Context can be passed down to any level, without having been specified in the
+ parent component. Almost like a wormhole. You just have to opt-in to the context by specifying
+ contextTypes
+  */
+ButtonChoices.contextTypes = {
+  store: PropTypes.object,
+};
+
 
 export default ButtonChoices;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PropTypes from 'prop-types';
 
 import './index.css';
+import tweetApp from './reducers/index';
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(tweetApp);
+
 ReactDOM.render(
-  <Routes />,
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
   document.getElementById('root'),
 );
+
+Provider.childContextTypes = {
+  store: PropTypes.object,
+};
 registerServiceWorker();
