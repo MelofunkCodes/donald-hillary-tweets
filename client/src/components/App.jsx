6fba9deb7dd62b0df98cf86c1792201da023f9fc
Melinda Yeoh <myeoh27@gmail.com>
2018-01-18 17:35:59 -0500

    Converted App component from class to factory function. Also specified propTypes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import PropTypes from 'prop-types';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="main-app">
-        <header className="main-header">
-          <h1><Link to="/">Donald-Hillary Tweets</Link></h1>
-          <div className="header-buttons">
-            <Link to="/">
-              <i className="fa fa-home fa-2x" aria-hidden="true" />
-            </Link>
-            <a
-              href="https://github.com/MelofunkCodes/donald-hillary-tweets"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github fa-2x" aria-hidden="true" />
-            </a>
-          </div>
-        </header>
-        <main className="main-content">
-          {this.props.children}
-        </main>
+const propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const App = props => (
+  <div className="main-app">
+    <header className="main-header">
+      <h1><Link to="/">Donald-Hillary Tweets</Link></h1>
+      <div className="header-buttons">
+        <Link to="/">
+          <i className="fa fa-home fa-2x" aria-hidden="true" />
+        </Link>
+        <a
+          href="https://github.com/MelofunkCodes/donald-hillary-tweets"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-github fa-2x" aria-hidden="true" />
+        </a>
       </div>
-    );
-  }
-}
+    </header>
+    <main className="main-content">
+      {props.children}
+    </main>
+  </div>
+);
+
+App.propTypes = propTypes;
 
 export default App;
