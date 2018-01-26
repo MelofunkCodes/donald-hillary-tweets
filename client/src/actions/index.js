75e30dd6d172a44335721252a66be6d6f3543d94
Melinda Yeoh <myeoh27@gmail.com>
2018-01-26 14:36:34 -0500

    Updated formatting of actions/index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,4 @@ export const FETCH_TWEETS = 'FETCH_TWEETS';
 export const FETCH_TWEETS_SUCCESS = 'FETCH_TWEETS_SUCCESS';
 
 export const fetchTweets = username => ({ type: FETCH_TWEETS, username });
-export const fetchTweetsSuccess = (tweets) => (
-  {
-    type: FETCH_TWEETS_SUCCESS,
-    tweets,
-  }
-);
+export const fetchTweetsSuccess = tweets => ({ type: FETCH_TWEETS_SUCCESS, tweets });
